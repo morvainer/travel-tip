e3a6915ed92f26a87c7fa953d83eaa01579cd58b
Niv Ivri <nivii123456@gmail.com>
2021-08-18 13:27:35 +0300

    add api

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -40,7 +40,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyDU7dioNfhFTADeUY7K0oPN80S90jOUBpI'
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
